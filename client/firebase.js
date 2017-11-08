2882dbd5bb462850c0f674a0287e25432403aa92
Tino Fuhrmann <tino-fuhrmann@web.de>
2017-11-08 13:31:41 -0800

    Added firebase-tools

---
+++ client/firebase.js
@@ -0,0 +1,10 @@
+const firebase = require("firebase");
+
+var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);
