4aaad04f36376ba746e8ba784dbf6a66b54ac72b
Andrew Luo <andrewjluo@gmail.com>
2017-11-08 15:33:57 -0800

    Moved webpack files into client/src

---
+++ client/src/assets/javascripts/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import $ from "jquery"
+
+import HelloWorld from "components/hello-world"
+
+
+ReactDOM.render(<HelloWorld />, $("#container")[0])
