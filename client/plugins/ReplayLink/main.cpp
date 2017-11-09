1f6e0d64baaf740915b81b65d15cd510fb173391
Krijn <querijn@irule.at>
2017-11-09 09:58:53 -0800

    Make a camera controller

--- client/plugins/ReplayLink/main.cpp
+++ client/plugins/ReplayLink/main.cpp
@@ -1,7 +1,10 @@
+#include "replaylink.h"
+
 #include <iostream>
 #include <chrono>
 #include <thread>
 
+#include <../Patterns/PatternSearch.h>
 #include <../Process/Process.h>
 
 #define PROCESS_NAME L"League of Legends.exe"
@@ -9,16 +12,114 @@
 using namespace blackbone;
 using namespace std::chrono_literals;
 
-int main() 
+namespace replay
 {
-	std::vector<DWORD> t_ProcessList;
+	Process league_process;
+	bool attached = false; 
+	
+	uint32_t camera_object_address = 0;
 
-	std::wcout << L"Waiting for '" << PROCESS_NAME << L"'." << std::endl;
-
-	// Wait for League of Legends to load
-	while (t_ProcessList.empty())
+	camera_impl::camera_impl()
 	{
-		t_ProcessList = Process::EnumByName(PROCESS_NAME);
+		std::vector<DWORD> t_ProcessList;
+
+		std::wcout << L"Waiting for '" << PROCESS_NAME << L"'." << std::endl;
+
+		// Wait for League of Legends to load
+		while (t_ProcessList.empty())
+		{
+			t_ProcessList = Process::EnumByName(PROCESS_NAME);
+			std::this_thread::sleep_for(2ms);
+		}
+
+		if (NT_SUCCESS(league_process.Attach(t_ProcessList[0])) == false)
+		{
+			std::wcerr << L"Error trying to open '" << PROCESS_NAME << L"'." << std::endl;
+			return;
+		}
+
+		attached = true;
+		std::cout << "Trying to find the pattern for the camera object..." << std::endl;
+
+		camera_object_address = 0;
+		PatternSearch pattern("\x8B\x0D\xCC\xCC\xCC\xCC\x8B\x49\x24\xE8\xCC\xCC\xCC\xCC\x8B");
+
+		std::vector<ptr_t> results;
+		pattern.SearchRemoteWhole(league_process, true, 0xCC, results);
+
+		// Unable to find position offset
+		if (results.size() == 0)
+		{
+			std::cout << "Unable to find the pattern." << std::endl;
+			return;
+		}
+
+		if (league_process.memory().Read<uint32_t>(results[0] + 2, camera_object_address))
+		{
+			std::cout << "Failed to read a part of the pattern." << std::endl;
+			return;
+		}
+
+		if (league_process.memory().Read<uint32_t>(camera_object_address, camera_object_address))
+		{
+			std::cout << "Failed to read in the multiplayer client." << std::endl;
+			return;
+		}
+
+		if (league_process.memory().Read<uint32_t>(camera_object_address + 12, camera_object_address))
+		{
+			std::cout << "Failed to get the camera object." << std::endl;
+			return;
+		}
+	}
+
+	vector2 camera_impl::get_position()
+	{
+		vector2 t_result(0, 0);
+		if (league_process.memory().Read<float>(camera_object_address + 0x12c, t_result.x))
+		{
+			std::cout << "Failed to read x position of camera." << std::endl;
+			return vector2(-1, -1);
+		}
+
+		if (league_process.memory().Read<float>(camera_object_address + 0x134, t_result.y))
+		{
+			std::cout << "Failed to read y position of camera." << std::endl;
+			return vector2(-1, -1);
+		}
+
+		return t_result;
+	}
+
+	void camera_impl::set_position(float x, float y)
+	{
+		if (league_process.memory().Write<float>(camera_object_address + 0x12c, x))
+		{
+			std::cout << "Failed to write x position of camera." << std::endl;
+			return;
+		}
+
+		if (league_process.memory().Write<float>(camera_object_address + 0x134, y))
+		{
+			std::cout << "Failed to write y position of camera." << std::endl;
+			return;
+		}
+	}
+
+	void camera_impl::set_position(vector2 vec)
+	{
+		return set_position(vec.x, vec.y);
+	}
+}
+
+int main()
+{
+	replay::camera_impl t_camera;
+	while (true)
+	{
+		auto t_vec = t_camera.get_position();
+
+		std::cout << "X = " << t_vec.x << ", Y = " << t_vec.y << std::endl;
 		std::this_thread::sleep_for(2ms);
 	}
 }
---
+++ client/plugins/ReplayLink/replaylink.h
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace replay
+{
+	struct vector2 
+	{ 
+		vector2(float a_x, float a_y) 
+			: x(a_x), y(a_y)
+		{
+		}
+
+		float x; float y; 
+	};
+
+	class camera
+	{
+	public:
+		camera() { }
+		virtual ~camera() { }
+
+		virtual vector2 get_position() = 0;
+		virtual void set_position(float x, float y) = 0;
+	};
+
+	class camera_impl : public camera
+	{
+	public:
+		camera_impl();
+		~camera_impl() { }
+
+		vector2 get_position();
+		void set_position(float x, float y);
+		void set_position(vector2 vec);
+
+	};
+};
